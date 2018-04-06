24318e825286e6eb02c309ec8a65d94be2cf166e
sunmg1994 <sunmg1994@gmail.com>
2018-04-06 11:33:35 +0800

    删除多余的文件夹

--- gulp-setting/src/js/file1.js
+++
@@ -1,7 +0,0 @@
-// Console Log 1
-console.log('This is file One');
-
-
-
-// Console Log 2
-console.log('This is file 2');
--- gulp-setting/src/js/file2.js
+++
@@ -1,7 +0,0 @@
-// Console Log
-
-console.log('This is file 2');
-
-// Alert
-
-alert('This is file 2');
